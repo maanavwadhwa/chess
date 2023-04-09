116717151879b2ac255288244209c19a6146abf2
Maanav Wadhwa <maanavwadhwa9@gmail.com>
2023-04-09 00:27:17 -0400

    condensed moves into direction helper functions and added bishop class

--- Chess.py
+++ Chess.py
@@ -1,6 +1,6 @@
 from cmu_graphics import *
 class Rook:
-    def __init__(self,rank, file, color): #rank --> row, file---> col
+    def __init__(self, rank, file, color): #rank --> row, file---> col
         self.rank = rank
         self.file = file
         self.color = color
@@ -10,47 +10,37 @@ class Rook:
     def move(self, newRank, newFile):
         self.rank = newRank
         self.file = newFile 
+  
+    def legalMoves(self, currStateBoard):
+
+        possibleMoves = (getRightMoves(self, currStateBoard)+
+                         getUpwardMoves(self, currStateBoard)+
+                         getDownwardMoves(self, currStateBoard)+
+                         getLeftMoves(self, currStateBoard))
+        return possibleMoves
+
+    def __repr__(self):
+        return f'{self.name}: ({self.rank},{self.file}, {self.color})'
+    
+class Bishop:
+    def __init__(self, rank, file, color): #rank --> row, file---> col
+        self.rank = rank
+        self.file = file
+        self.color = color 
+        self.image = f'Images/bishop_{self.color}.png'
+        self.name = 'Bishop'
+
+    def move(self, newRank, newFile):
+        self.rank = newRank
+        self.file = newFile 
         
     def legalMoves(self, currStateBoard):
-        possibleMoves = []
-        rows, cols = len(currStateBoard),len(currStateBoard[0])
+        #returns all legal moves for the bishop
 
-        #checking right moves available for rook
-        for i in range(self.file+1,cols):
-            if currStateBoard[self.rank][i]!=None:
-                if currStateBoard[self.rank][i].color != self.color:
-                    possibleMoves.append((self.rank, i))
-                break
-            else:
-                possibleMoves.append((self.rank, i))
-
-        #check left moves available for rook
-        for i in range(self.file-1, -1, -1):
-            if currStateBoard[self.rank][i]!=None:
-                if currStateBoard[self.rank][i].color != self.color:
-                    possibleMoves.append((self.rank, i))
-                break
-            else:
-                possibleMoves.append((self.rank, i))
-
-        #check upward moves available for rook
-        for i in range(self.rank-1,-1,-1):
-            if currStateBoard[i][self.file]!=None:
-                if currStateBoard[i][self.file].color != self.color:
-                    possibleMoves.append((i,self.file))
-                break
-            else:
-                possibleMoves.append((i,self.file))
-
-        #check downward moves available for rook
-        for i in range(self.rank+1,rows):
-            if currStateBoard[i][self.file]!=None:
-                if currStateBoard[i][self.file].color != self.color:
-                    possibleMoves.append((i,self.file))
-                break
-            else:
-                possibleMoves.append((i,self.file))
-  
+        possibleMoves = (getUpwardRightMoves(self, currStateBoard)+
+                         getUpwardLeftMoves(self, currStateBoard)+
+                         getDownwardRightMoves(self, currStateBoard)+
+                         getDownwardLeftMoves(self, currStateBoard))
         return possibleMoves
 
     def __repr__(self):
@@ -66,6 +56,118 @@ class Rook:
 #                 return True
 #     return False
 
+def getUpwardMoves(piece, currStateBoard):
+    possibleMoves = []
+    for i in range(piece.rank-1,-1,-1):
+        if currStateBoard[i][piece.file]!=None:
+            if currStateBoard[i][piece.file].color != piece.color:
+                possibleMoves.append((i,piece.file))
+            break
+        else:
+            possibleMoves.append((i,piece.file))
+    return possibleMoves
+
+def getDownwardMoves(piece, currStateBoard):
+    possibleMoves = []
+    rows = len(currStateBoard)
+    for i in range(piece.rank+1,rows):
+        if currStateBoard[i][piece.file]!=None:
+            if currStateBoard[i][piece.file].color != piece.color:
+                possibleMoves.append((i,piece.file))
+            break
+        else:
+            possibleMoves.append((i,piece.file))
+    return possibleMoves
+
+def getLeftMoves(piece, currStateBoard):
+    possibleMoves = []
+    for i in range(piece.file-1, -1, -1):
+        if currStateBoard[piece.rank][i]!=None:
+            if currStateBoard[piece.rank][i].color != piece.color:
+                possibleMoves.append((piece.rank, i))
+            break
+        else:
+            possibleMoves.append((piece.rank, i))
+    return possibleMoves
+
+def getRightMoves(piece, currStateBoard):
+    possibleMoves = []
+    cols = len(currStateBoard[0])
+    for i in range(piece.file+1,cols):
+        if currStateBoard[piece.rank][i]!=None:
+            if currStateBoard[piece.rank][i].color != piece.color:
+                possibleMoves.append((piece.rank, i))
+            break
+        else:
+            possibleMoves.append((piece.rank, i))
+    return possibleMoves
+
+def getUpwardRightMoves(piece, currStateBoard):
+    possibleMoves = []
+    cols = len(currStateBoard[0])
+    j = 1 #represents a way to increment the row relative to its starting row
+    for i in range(piece.file+1, cols):
+        #prevents piece from being able to position itself to a row above the board 
+        if piece.rank-j<0: 
+            break
+        elif currStateBoard[piece.rank-j][i]!=None:
+            if currStateBoard[piece.rank-j][i].color != piece.color:
+                possibleMoves.append((piece.rank-j, i))
+            break
+        else:
+            possibleMoves.append((piece.rank-j, i))
+        j+=1
+    return possibleMoves
+
+def getUpwardLeftMoves(piece, currStateBoard):
+    possibleMoves = []
+    j =1
+    for i in range(piece.file-1, -1, -1):
+        #prevents piece from being able to position itself to a row above the board 
+        if piece.rank - j < 0: 
+            break
+        elif currStateBoard[piece.rank-j][i]!=None:
+            if currStateBoard[piece.rank-j][i].color != piece.color:
+                possibleMoves.append((piece.rank-j, i))
+            break
+        else:
+            possibleMoves.append((piece.rank-j, i))
+        j+=1
+    return possibleMoves
+
+def getDownwardRightMoves(piece, currStateBoard):
+    possibleMoves = []
+    rows, cols = len(currStateBoard), len(currStateBoard[0])
+    j = 1
+    for i in range(piece.file+1, cols): 
+        #prevents piece from being able to position itself to a row below the board 
+        if piece.rank+j>=rows: 
+            break
+        elif currStateBoard[piece.rank+j][i]!=None: #7,3
+            if currStateBoard[piece.rank+j][i].color != piece.color:
+                possibleMoves.append((piece.rank+j,i))
+            break
+        else:
+            possibleMoves.append((piece.rank+j, i))
+        j+=1
+    return possibleMoves
+
+def getDownwardLeftMoves(piece, currStateBoard):
+    possibleMoves = []
+    rows = len(currStateBoard)
+    j = 1
+    for i in range(piece.file-1, -1, -1):
+        #prevents piece from being able to position itself to a row below the board 
+        if piece.rank +j>=rows: 
+            break
+        elif currStateBoard[piece.rank+j][i]!=None:
+            if currStateBoard[piece.rank+j][i].color != piece.color:
+                possibleMoves.append((piece.rank+j,i))
+            break
+        else:
+            possibleMoves.append((piece.rank+j, i))
+        j+=1
+    return possibleMoves
 
 def onAppStart(app):
     app.rows = 8
@@ -87,6 +189,10 @@ def initialBoard(rows, cols):
                 board[row][col] = Rook(row,col,'black')
             elif (row,col) in [(7,0),(7,7)]:
                 board[row][col] = Rook(row,col,'white')
+            elif (row, col) in [(0,2),(0,5)]:
+                board[row][col] = Bishop(row,col,'black')
+            elif (row,col) in [(7,2),(7,5)]:
+                board[row][col] = Bishop(row,col,'white')
     return board 
 
 def redrawAll(app):
@@ -177,8 +283,11 @@ def getCellFromPoint(app, mouseX, mouseY):
 def onMousePress(app, mouseX, mouseY):
     cellLocation = getCellFromPoint(app, mouseX, mouseY)
 
+    if cellLocation == None:
+        app.selectedCell = None
+
     # if I am not highlighting a cell and i click on a piece
-    if app.selectedCell == None and app.board[cellLocation[0]][cellLocation[1]]!=None:
+    elif app.selectedCell == None and app.board[cellLocation[0]][cellLocation[1]]!=None:
         currPiece = app.board[cellLocation[0]][cellLocation[1]]
         app.showMoves = True
         app.selectedCell = cellLocation
@@ -200,6 +309,13 @@ def onMousePress(app, mouseX, mouseY):
 
     #if I click somewhere on the board and that space contains a piece
     elif cellLocation!=None and app.board[cellLocation[0]][cellLocation[1]]!=None:
+        if app.selectedCell!=None:
+            currPiece = app.board[app.selectedCell[0]][app.selectedCell[1]]
+            if (cellLocation) in currPiece.legalMoves(app.board):
+                currPiece.move(cellLocation[0], cellLocation[1])
+                app.board[app.selectedCell[0]][app.selectedCell[1]] = None
+                app.board[cellLocation[0]][cellLocation[1]] = currPiece
+                app.selectedCell = None
         app.selectedCell = cellLocation 
 
 def main():
